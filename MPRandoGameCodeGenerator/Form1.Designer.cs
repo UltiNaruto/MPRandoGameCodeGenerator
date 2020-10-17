dc30b27dfcb9b7931ed2ecb60c1b6a5e9b5ea158
NatsuDragnir <zenaruto@ymail.com>
2020-10-18 01:26:22 +0200

    Improved old generated games support Added copy gamesettings config choice support Having no list for the prime game you want will search for old generated games in the game settings from Dolphin

--- MPRandoGameCodeGenerator/Form1.Designer.cs
+++ MPRandoGameCodeGenerator/Form1.Designer.cs
@@ -37,8 +37,12 @@
             this.ISOtoPatch_txtBox = new System.Windows.Forms.TextBox();
             this.Patch_btn = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -47,26 +51,27 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.ISOtoPatch_lbl, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.Patch_btn, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Patch_btn, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(482, 91);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(363, 106);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // ISOtoPatch_lbl
             // 
             this.ISOtoPatch_lbl.AutoSize = true;
-            this.ISOtoPatch_lbl.Location = new System.Drawing.Point(3, 3);
-            this.ISOtoPatch_lbl.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.ISOtoPatch_lbl.Location = new System.Drawing.Point(2, 2);
+            this.ISOtoPatch_lbl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 0);
             this.ISOtoPatch_lbl.Name = "ISOtoPatch_lbl";
-            this.ISOtoPatch_lbl.Size = new System.Drawing.Size(94, 17);
+            this.ISOtoPatch_lbl.Size = new System.Drawing.Size(73, 13);
             this.ISOtoPatch_lbl.TabIndex = 3;
             this.ISOtoPatch_lbl.Text = "ISO to patch :";
             this.toolTip1.SetToolTip(this.ISOtoPatch_lbl, resources.GetString("ISOtoPatch_lbl.ToolTip"));
@@ -75,24 +80,25 @@
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel2.Controls.Add(this.ISOtoPatch_btn, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.ISOtoPatch_txtBox, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 20);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(482, 32);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(362, 26);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
             // ISOtoPatch_btn
             // 
             this.ISOtoPatch_btn.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ISOtoPatch_btn.Location = new System.Drawing.Point(445, 3);
+            this.ISOtoPatch_btn.Location = new System.Drawing.Point(334, 2);
+            this.ISOtoPatch_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ISOtoPatch_btn.Name = "ISOtoPatch_btn";
-            this.ISOtoPatch_btn.Size = new System.Drawing.Size(34, 26);
+            this.ISOtoPatch_btn.Size = new System.Drawing.Size(26, 22);
             this.ISOtoPatch_btn.TabIndex = 0;
             this.ISOtoPatch_btn.Text = "...";
             this.ISOtoPatch_btn.UseVisualStyleBackColor = true;
@@ -100,42 +106,87 @@
             // 
             // ISOtoPatch_txtBox
             // 
-            this.ISOtoPatch_txtBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ISOtoPatch_txtBox.Location = new System.Drawing.Point(3, 6);
-            this.ISOtoPatch_txtBox.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.ISOtoPatch_txtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ISOtoPatch_txtBox.Location = new System.Drawing.Point(2, 3);
+            this.ISOtoPatch_txtBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 2);
             this.ISOtoPatch_txtBox.MaxLength = 260;
             this.ISOtoPatch_txtBox.Name = "ISOtoPatch_txtBox";
-            this.ISOtoPatch_txtBox.Size = new System.Drawing.Size(436, 22);
+            this.ISOtoPatch_txtBox.Size = new System.Drawing.Size(328, 20);
             this.ISOtoPatch_txtBox.TabIndex = 1;
             // 
             // Patch_btn
             // 
             this.Patch_btn.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Patch_btn.Location = new System.Drawing.Point(3, 59);
+            this.Patch_btn.Location = new System.Drawing.Point(2, 74);
+            this.Patch_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Patch_btn.Name = "Patch_btn";
-            this.Patch_btn.Size = new System.Drawing.Size(476, 29);
+            this.Patch_btn.Size = new System.Drawing.Size(359, 30);
             this.Patch_btn.TabIndex = 4;
             this.Patch_btn.Text = "Patch";
             this.Patch_btn.UseVisualStyleBackColor = true;
             this.Patch_btn.Click += new System.EventHandler(this.Patch_btn_Click);
             // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 135F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 46);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(363, 26);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Copy vanilla game config";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Disabled",
+            "In GameSettings folder",
+            "Next to the ISO"});
+            this.comboBox1.Location = new System.Drawing.Point(138, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Disabled";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 91);
+            this.ClientSize = new System.Drawing.Size(363, 106);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(500, 138);
+            this.MaximumSize = new System.Drawing.Size(379, 145);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(500, 138);
+            this.MinimumSize = new System.Drawing.Size(379, 145);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Prime Rando Game Code Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -149,6 +200,9 @@
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label ISOtoPatch_lbl;
         private System.Windows.Forms.Button Patch_btn;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- MPRandoGameCodeGenerator/Form1.cs
+++ MPRandoGameCodeGenerator/Form1.cs
@@ -1,21 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace MPRandoGameCodeGenerator
 {
     public partial class Form1 : Form
     {
-        private Random rand = null;
-        private const String AllowedCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private const String MP1BaseString = "GM8";
-        private const String MP2BaseString = "G2M";
+        Random rand = null;
+        const String MP1BaseString = "GM8";
+        const String MP2BaseString = "G2M";
+        readonly String DolphinConfigFolder;
+        readonly String GameSettingsFolder;
         public Form1()
         {
-            rand = new Random((int)(new DateTime().Ticks / TimeSpan.TicksPerMillisecond));
             InitializeComponent();
+            DolphinConfigFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + Path.DirectorySeparatorChar + "Dolphin Emulator";
+            GameSettingsFolder = DolphinConfigFolder + Path.DirectorySeparatorChar + "GameSettings";
+        }
+
+        List<String> GetAllGeneratedDevCodes(MPType type, String GameRegion)
+        {
+            var Dir = default(String);
+
+            if (type == MPType.PRIME)
+            {
+                if (File.Exists("ugc_mp1.list"))
+                    return File.ReadAllLines("ugc_mp1.list").ToList();
+
+                Dir = DolphinConfigFolder + Path.DirectorySeparatorChar + "GameSettings";
+
+                if (!Directory.Exists(Dir))
+                    return new List<String>();
+                return Directory.EnumerateFiles(
+                    Dir,
+                    MP1BaseString + GameRegion + "*.ini"
+                ).Select((str) =>
+                {
+                    var match = Regex.Match(str, @"[A-Z0-9]{6}\.ini");
+                    if (match.Success)
+                        return match.Value.Substring(4, 2);
+                    return str;
+                }).ToList();
+            }
+
+            if (type == MPType.PRIME_2)
+            {
+                if (File.Exists("ugc_mp2.list"))
+                    return File.ReadAllLines("ugc_mp2.list").ToList();
+
+                Dir = DolphinConfigFolder + Path.DirectorySeparatorChar + "GameSettings";
+
+                if (!Directory.Exists(Dir))
+                    return new List<String>();
+                return Directory.EnumerateFiles(
+                    Dir,
+                    MP2BaseString + GameRegion + "*.ini"
+                ).Select((str) =>
+                {
+                    var match = Regex.Match(str, @"[A-Z0-9]{6}\.ini");
+                    if (match.Success)
+                        return match.Value.Substring(4, 2);
+                    return str;
+                }).ToList();
+            }
+
+            throw new Exception("Game unsupported");
+        }
+
+        String RandomizeGameCode(MPType type, String GameRegion, bool saveToList=false)
+        {
+            const String AllowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const int AllowedCharsLen = 36;
+
+            var devCode = default(String);
+            var usedGameCodes = default(List<String>);
+
+            devCode = "01";
+            if (saveToList)
+                usedGameCodes = GetAllGeneratedDevCodes(type, GameRegion);
+            else
+                usedGameCodes = new List<String>();
+
+            // Reserved for vanilla game
+            if (!usedGameCodes.Contains("01"))
+                usedGameCodes.Add("01");
+
+            switch (type)
+            {
+                case MPType.PRIME:
+                    if (GameRegion == "E")
+                    {
+                        // Reserved for Practice Mod
+                        if (!usedGameCodes.Contains("PM"))
+                            usedGameCodes.Add("PM");
+                    }
+                    if (GameRegion == "P")
+                    {
+                        // Reserved for Scan Dash Enabled version
+                        if (!usedGameCodes.Contains("SD"))
+                            usedGameCodes.Add("SD");
+                    }
+                    break;
+                case MPType.PRIME_2:
+                default:
+                    break;
+            }
+
+            while(usedGameCodes.Contains(devCode))
+                devCode = new String(new char[] { AllowedChars[rand.Next(AllowedCharsLen)], AllowedChars[rand.Next(AllowedCharsLen)] });
+            usedGameCodes.Add(devCode);
+
+            if (usedGameCodes.Contains("01"))
+                usedGameCodes.Remove("01");
+
+            switch (type)
+            {
+                case MPType.PRIME:
+                    if (GameRegion == "E")
+                    {
+                        if (usedGameCodes.Contains("PM"))
+                            usedGameCodes.Remove("PM");
+                    }
+                    if (GameRegion == "P")
+                    {
+                        if (usedGameCodes.Contains("SD"))
+                            usedGameCodes.Remove("SD");
+                    }
+                    if(saveToList)
+                        File.WriteAllLines("ugc_mp1_"+GameRegion.ToLower()+".list", usedGameCodes);
+                    return MP1BaseString + GameRegion + devCode;
+                case MPType.PRIME_2:
+                    if(saveToList)
+                        File.WriteAllLines("ugc_mp2_" + GameRegion.ToLower() + ".list", usedGameCodes);
+                    return MP2BaseString + GameRegion + devCode;
+                default:
+                    throw new Exception("Game unsupported");
+            }
         }
 
         private void ISOtoPatch_btn_Click(object sender, EventArgs e)
@@ -34,22 +157,23 @@ namespace MPRandoGameCodeGenerator
         private void Patch_btn_Click(object sender, EventArgs e)
         {
             String GameCode = String.Empty;
+            String GameRegion = String.Empty;
             String TargetGameCode = String.Empty;
-            String SaveStateFolder = String.Empty;
-            String CurGameCode = String.Empty;
-            String GameSettingsFolder = String.Empty;
-            String DolphinConfigFolder = String.Empty;
             MPType GameType = MPType.UNK;
-            List<String> usedGameCodes = new List<String>();
+            String TargetConfigFile = String.Empty;
 
-            usedGameCodes.Add("01");
-            DolphinConfigFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + Path.DirectorySeparatorChar + "Dolphin Emulator";
             if (this.ISOtoPatch_txtBox.Text == "")
             {
                 MessageBox.Show("Set the path of the ISO you want to generate a game code");
                 return;
             }
             GameCode = ISOManager.GetISOGameCode(this.ISOtoPatch_txtBox.Text);
+            if (GameCode == "ERROR")
+            {
+                MessageBox.Show("This is not a GC ISO");
+                return;
+            }
+            GameRegion = GameCode.Substring(3, 1);
             if (GameCode.Substring(0, 3) == MP1BaseString)
                 GameType = MPType.PRIME;
             else if (GameCode.Substring(0, 3) == MP2BaseString)
@@ -59,29 +183,36 @@ namespace MPRandoGameCodeGenerator
                 MessageBox.Show("This game is not Metroid Prime 1 or 2!");
                 return;
             }
-            if (Directory.Exists(DolphinConfigFolder))
+            TargetGameCode = RandomizeGameCode(GameType, GameRegion, comboBox1.Text == comboBox1.Items[1].ToString());
+            if (comboBox1.Text != comboBox1.Items[0].ToString())
             {
-                SaveStateFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + Path.DirectorySeparatorChar + "Dolphin Emulator\\StateSaves";
-                foreach (String file in Directory.EnumerateFiles(SaveStateFolder, GameType == MPType.PRIME ? MP1BaseString + "*.s*" : MP2BaseString + "*.s*", SearchOption.TopDirectoryOnly))
+                if (Directory.Exists(GameSettingsFolder))
                 {
-                    CurGameCode = Path.GetFileNameWithoutExtension(file);
-                    if (CurGameCode != GameCode)
-                        if (!usedGameCodes.Contains(CurGameCode.Substring(4)))
-                            usedGameCodes.Add(CurGameCode.Substring(4));
+                    if (File.Exists(GameSettingsFolder + Path.DirectorySeparatorChar + GameCode.Substring(0, 4) + "01.ini"))
+                    {
+                        if (comboBox1.Text == comboBox1.Items[1].ToString())
+                        {
+                            TargetConfigFile = GameSettingsFolder + "\\" + TargetGameCode + ".ini";
+                            File.Copy(GameSettingsFolder + Path.DirectorySeparatorChar + GameCode.Substring(0, 4) + "01.ini", TargetConfigFile);
+                            File.WriteAllText(TargetConfigFile, File.ReadAllText(TargetConfigFile) + "\r\n\r\n# " + Path.GetFileNameWithoutExtension(this.ISOtoPatch_txtBox.Text));
+                        }
+                        if (comboBox1.Text == comboBox1.Items[2].ToString())
+                        {
+                            TargetConfigFile = Path.GetDirectoryName(this.ISOtoPatch_txtBox.Text) + Path.DirectorySeparatorChar + TargetGameCode + ".ini";
+                            File.Copy(GameSettingsFolder + Path.DirectorySeparatorChar + GameCode.Substring(0, 4) + "01.ini", TargetConfigFile);
+                            File.WriteAllText(TargetConfigFile, File.ReadAllText(TargetConfigFile) + "\r\n\r\n# " + Path.GetFileNameWithoutExtension(this.ISOtoPatch_txtBox.Text));
+                        }
+                    }
                 }
-            }
-            TargetGameCode = GameCode.Substring(0, 4) + AllowedCharacters[rand.Next(AllowedCharacters.Length)] + "" + AllowedCharacters[rand.Next(AllowedCharacters.Length)];
-            while (usedGameCodes.Contains(TargetGameCode.Substring(4)))
-                TargetGameCode = GameCode.Substring(0, 4) + AllowedCharacters[rand.Next(AllowedCharacters.Length)] + "" + AllowedCharacters[rand.Next(AllowedCharacters.Length)];
-            if (Directory.Exists(DolphinConfigFolder))
-            {
-                GameSettingsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + Path.DirectorySeparatorChar + "Dolphin Emulator\\GameSettings";
-                if (File.Exists(GameSettingsFolder + "\\" + GameCode + ".ini"))
-                    File.Copy(GameSettingsFolder + "\\" + GameCode + ".ini", GameSettingsFolder + "\\" + TargetGameCode + ".ini");
             }
             ISOManager.PatchGameCode(this.ISOtoPatch_txtBox.Text, TargetGameCode);
             ISOManager.PatchGameTitle(this.ISOtoPatch_txtBox.Text, Path.GetFileNameWithoutExtension(this.ISOtoPatch_txtBox.Text));
             MessageBox.Show("Game patched with Game Code \"" + TargetGameCode + "\"\r\nand with Game Title \"" + Path.GetFileNameWithoutExtension(this.ISOtoPatch_txtBox.Text) + "\"");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = new Random((int)(DateTimeOffset.Now.ToUnixTimeSeconds()));
         }
     }
 }
